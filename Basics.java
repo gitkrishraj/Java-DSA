7348d1e2763fd607c4af48bca2b8cd1401e62bb3
gitkrishraj <krishraj30032005@gmail.com>
2025-01-04 14:25:38 +0530

    third commit

---
+++ Basics.java
@@ -0,0 +1,22 @@
+public class Basics {
+public static void main(String[] args) {
+    // if loop
+    // int a =10;
+    // if (a==10){
+    //     System.out.println("True");
+    // }
+
+    // while loop
+    // int count = 1;
+    // while (count != 5){
+    //     System.out.println(count);
+    //     count++;
+    // }
+
+    //for loop
+    for(int count=1; count !=5; count++){
+     System.out.println(count);
+    }
+}
+    
+}
